6568fdd41a202a25400e04a9fa56f41af43c4a17
User <ak42490021@gmail.com>
2022-12-20 13:40:41 +0100

    figure,For

---
+++ src/homework/homework1/FigurePainter.java
@@ -0,0 +1,44 @@
+package homework.homework1;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+        for (int i = 5; i > 0; i--) {
+            System.out.println(' ');
+            for (int j = 0; j < i; j++) {
+                System.out.print('*' + " ");
+            }
+        }
+        for (int i = -5; i < 0; i++) {
+            System.out.println(' ');
+            for (int j = -5; j <= i; j++) {
+                System.out.print('*' + " ");
+
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(' ');
+            for (int j = 4; j > i; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+
+        }
+        for (int i = 0; i < 6; i++) {
+            System.out.println(' ');
+            for (int j = 6; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("* ");
+            }
+
+        }
+
+
+    }
+
+}
+
+
---
+++ src/homework/homework1/IfForExample.java
@@ -0,0 +1,46 @@
+package homework.homework1;
+
+public class IfForExample {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 40;
+        if (a > b) {
+            System.out.println(a);
+        }
+        if (b > a) {
+            System.out.println(b);
+
+        }
+        //այս առայադրանքի ճիշտ կոդը չգտա
+
+        System.out.println(' ');
+
+
+        int x = 5;
+        int y = 10;
+        for (int i = x; i <= y; i++) {
+            System.out.print((i) + "  ");
+        }
+
+        System.out.println("  ");
+
+        a = 88;
+        b = 89;
+        char c;
+        c = (char) a;
+        System.out.print(c + "  ");
+        c = (char) b;
+        System.out.println(c);
+
+
+        a = 15;
+        b = 20;
+        boolean q;
+        q = (a == b);
+        System.out.println(q);
+        //երևի կիսատ է!
+
+
+    }
+
+}
