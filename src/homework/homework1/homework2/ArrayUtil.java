af7b1b418b119cc23ff03596640dee49c9869e0a
User <ak42490021@gmail.com>
2022-12-21 11:37:58 +0100

    homework 2

--- src/homework/homework1/homework2/ArrayUtil.java
+++ src/homework/homework1/homework2/ArrayUtil.java
@@ -2,9 +2,9 @@ package homework.homework1.homework2;
 
 public class ArrayUtil {
     public static void main(String[] args) {
-        int []array={2,5,8,10,32,22,11,66,12,30};
-        for (int i = 0; i < array.length ; i++) {
-            System.out.print(array[i]+" ");
+        int[] array = {2, 5, 8, 10, 32, 22, 11, 66, 12, 30};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
 
 
         }
